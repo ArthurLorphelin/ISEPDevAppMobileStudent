48eb45aee0701475bad8ac69d5e4ce1576ea076b
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2024-06-02 14:31:59 +0200

    Finish the Activities : - incorrect password - user not in DB

--- app/src/main/java/com/example/isepdevappmobilestudent/activity/IncorrectPassword.java
+++ app/src/main/java/com/example/isepdevappmobilestudent/activity/IncorrectPassword.java
@@ -1,6 +1,9 @@
 package com.example.isepdevappmobilestudent.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +20,14 @@ public class IncorrectPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.incorrect_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button backToSignInPageButton = findViewById(R.id.back_to_sign_in_page_button_because_incorrect_password);
+        backToSignInPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backToSignInPageIntent = new Intent(getApplicationContext(), SignIn.class);
+                startActivity(backToSignInPageIntent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/isepdevappmobilestudent/activity/UserNotInDB.java
+++ app/src/main/java/com/example/isepdevappmobilestudent/activity/UserNotInDB.java
@@ -1,6 +1,9 @@
 package com.example.isepdevappmobilestudent.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +20,14 @@ public class UserNotInDB extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.user_not_in_db);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button backToSignInPageButton = findViewById(R.id.back_to_sign_in_page_button_because_user_not_in_db);
+        backToSignInPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignIn.class);
+                startActivity(intent);
+            }
         });
     }
 }
